fd9ce94e0fd04a7665bdc009caccab1d673b6d55
oana6678 <119964780+oana6678@users.noreply.github.com>
2022-12-06 20:31:14 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.A, function () {
+    muzica(120, 120)
+})
+input.onButtonPressed(Button.AB, function () {
+    muzica(1, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    muzica(60, 60)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    muzica(200, 200)
+})
+function muzica (tempo: number, volum: number) {
+    music.setVolume(volum)
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("C5 B A G F E D C ", tempo)
+    }
+}
 basic.forever(function () {
 	
 })
